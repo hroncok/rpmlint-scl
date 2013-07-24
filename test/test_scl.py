cfabcc16e1de0f6405ba84f9269ab485c5e4a2b0
Miro Hrončok <miro@hroncok.cz>
2013-07-24 20:57:36 +0200

    Created first test

---
+++ test/test_scl.py
@@ -0,0 +1,22 @@
+import sys, os
+# add rpmlint-scl, rpmlint and rpmlint/tools to PATH
+# also add rpmlint-scl/tools, so this keeps working once merged with rpmlint
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../rpmlint/tools'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../rpmlint'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'../tools'))
+sys.path.insert(0,os.path.join(os.path.dirname(__file__),'..'))
+
+# rpmlint's Testing need's TESTPATH
+os.environ['TESTPATH'] = os.path.dirname(__file__)
+
+
+import Testing
+import SCLCheck
+
+class TestSCL:
+    def test_silent(self):
+        '''SCL check on non-SCL spec has to be silent'''
+        self.pkg = Testing.getTestedSpecPackage('SpecCheck')
+        Testing.startTest()
+        SCLCheck.check.check_spec(self.pkg, self.pkg.name)
+        assert not len(Testing.getOutput())
